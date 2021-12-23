c55839ac712a24ba6361d76b7d447221e7d3b2f2
Zodd13 <berniam.pro@gmail.com>
2021-12-23 14:10:33 +0100

    fonction pour changer les quantités de l'input en place.

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,17 +4,22 @@ let localStorageProduct = JSON.parse(localStorage.getItem("product"));
 // getCartItem cible cart__items dans le html
 const getCartItem = document.getElementById('cart__items');
 
+// Fonction qui ajoute les produits dans le panier avec le HTML
 
 function addProductToCart() {
   let fullCart = [];
   let totalCart = 0;
   let totalFullCart = 0;
+
+  // UserHashMap filtre les doublons de produits ayant la même couleur
   const userHashMap = {}
   localStorageProduct = localStorageProduct.filter ((item, _)=>{
     let alreadyExists = userHashMap.hasOwnProperty(item.couleurDuProduit)
     return alreadyExists? false : userHashMap[item.couleurDuProduit] = 1
   })
-  // tant que i est plus petit que localStorageProduct.length on incrémente
+
+  // Boucle qui ajoute les produits tant qu'il y en a dans le local storage
+  // totalFullCart & totalCart font le calcul des produits du panier
   for(i = 0; i < localStorageProduct.length; i++){
   totalFullCart += parseInt (localStorageProduct[i].nombreDeProduit,10)
   totalCart += localStorageProduct[i].nombreDeProduit*localStorageProduct[i].prixDuProduit
@@ -50,30 +55,56 @@ function addProductToCart() {
 
 addProductToCart ();
 
-
+// Fonction permettant de supprimer les produits du panier
 function deleteItemProduct(){
   const deleteButton = document.querySelectorAll('.cart__item__content__settings__delete > .deleteItem');
-  for (let k = 0; k < localStorageProduct.length; k ++){
-    deleteButton[k].addEventListener('click', (e) => {
-      e.preventDefault();
-      let supprimerId = localStorageProduct[k].idDuProduit;
-      let supprimerCouleur = localStorageProduct[k].couleurDuProduit;
+  
+  // Boucle dans le local storage et ajoute un évènement au clique pour supprimer les produits séléctionnés
 
+  for (let i = 0; i < localStorageProduct.length; i ++){
+    deleteButton[i].addEventListener('click', (e) => {
+      e.preventDefault();
+
+      let supprimerId = localStorageProduct[i].idDuProduit;
+      let supprimerCouleur = localStorageProduct[i].couleurDuProduit;
+
+      // Filtre les produits n'ayant pas la même ID ou même couleur que l'élément cliqué
       localStorageProduct = localStorageProduct.filter(el => el.idDuProduit !== supprimerId || el.couleurDuProduit !== supprimerCouleur )
       
+      // Effectue les modification dans le local storage en transformant en chaine de caractère grâce à stringify
       localStorage.setItem("product",JSON.stringify(localStorageProduct));
       
+      // Pop-up alerte indiquant à l'usager que le produit séléctionné a bien été supprimer
       alert('Cette élement a bien été supprimer du panier');
+
+      // Rechargement de la page pour prendre en compte les modifications
       location.reload();
     })
   }
 }
-
+// Appel de la fonction deleteItemProduct
 deleteItemProduct();
 
-function changeQuantity (){
-  const changeButtonQty = document.getElementsByName('.itemQuantity')
-  changeButtonQty.addEventListener('click', function(){
-    console.log('salut')
-  })
-}
+// Fonction permettant de changer de quantité dans le panier
+function changeQty (){
+  const targetQty = document.querySelectorAll('.itemQuantity');
+    for (let i = 0; i < targetQty.length; i++){
+        targetQty[i].addEventListener('input', function(){
+        let valueInStorage = localStorageProduct[i].nombreDeProduit;
+        let changeQty = targetQty[i].value;
+        let colorProduct = localStorageProduct[i].couleurDuProduit;
+        
+        const newValue = localStorageProduct.find((el) => el.changeQty !== valueInStorage || el.changeQty === colorProduct) //!\\ Si on prends un canapé même ID mais couleur différent le deuxième modifie les quantité du premier canapé !!
+        // Valeur à insérer dans local storage = valeur indiqué dans l'input
+        newValue.nombreDeProduit = changeQty;
+        // Modifie la valeur de nombreDeProduit par la nouvelle valeur de l'input
+        localStorageProduct[i].nombreDeProduit = newValue.nombreDeProduit;
+
+        localStorage.setItem("product", JSON.stringify(localStorageProduct));
+        location.reload();
+        console.log(changeQty)
+      });
+    };
+};
+
+changeQty();
